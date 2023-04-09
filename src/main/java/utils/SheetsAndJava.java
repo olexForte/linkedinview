541aa857dd07aca6bde80a814452624b7766c7c7
evgeniy.dolbik <evgeniy.dolbik@fortegrp.com>
2023-04-09 17:00:27 +0200

    google Sheets implementation updates

--- src/main/java/utils/SheetsAndJava.java
+++ src/main/java/utils/SheetsAndJava.java
@@ -20,13 +20,12 @@ import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.List;
 
-//import com.google.auth.http.HttpCredentialsAdapter;
-//import com.google.auth.oauth2.GoogleCredentials;
-
 public class SheetsAndJava {
     private static Sheets sheetsService;
+    private static String SPREADSHEET_ID;
+    private static List<List<Object>> sheetValues;
     private static String SERVICE_NAME = "GS Example";
-    private static String SPREADSHEET_ID = "1RMYw31bO7M6GX9GCkwHRi4_AmPZ0h_ZEyu8cQllp8f4";
+    private static String SPREADSHEET_ID_TEST = "1RMYw31bO7M6GX9GCkwHRi4_AmPZ0h_ZEyu8cQllp8f4";
 
     private static Credential authorize() throws IOException, GeneralSecurityException {
         InputStream in = SheetsAndJava.class.getResourceAsStream("/credential.json");
@@ -58,21 +57,47 @@ public class SheetsAndJava {
 
     public static void main(String[] args) throws IOException, GeneralSecurityException{
         sheetsService = getSheetsService();
-        String range = "testSh!A2:B";
+        String range = "testSh!A:B";
+
+        sheetValues = getData_Param_ID_Range(SPREADSHEET_ID_TEST, range);
+
+
+        if(sheetValues == null || sheetValues.isEmpty()){
+            System.out.println("No data found. ");
+        } else {
+            for(List row : sheetValues){
+                System.out.printf("%s value 1 %s value 2 \n", row.get(0), row.get(1));
+            }
+        }
+    }
+
+    public static void setData(String[] args) throws IOException, GeneralSecurityException{
+        sheetsService = getSheetsService();
+        String range = "testSh!A:B";
+
+        sheetValues = getData_Param_ID_Range(SPREADSHEET_ID_TEST, range);
+
+
+        if(sheetValues == null || sheetValues.isEmpty()){
+            System.out.println("No data found. ");
+        } else {
+            for(List row : sheetValues){
+                System.out.printf("%s value 1 %s value 2 \n", row.get(0), row.get(1));
+            }
+        }
+    }
+
+
+    public static List<List<Object>> getData_Param_ID_Range(String spreadSheetId, String range) throws IOException, GeneralSecurityException{
+        sheetsService = getSheetsService();
+        SPREADSHEET_ID = spreadSheetId;
 
         ValueRange response = sheetsService.spreadsheets().values()
                 .get(SPREADSHEET_ID, range)
                 .execute();
 
-        List<List<Object>> values = response.getValues();
+        return response.getValues();
 
-        if(values == null || values.isEmpty()){
-            System.out.println("No data found. ");
-        } else {
-            for(List row : values){
-                System.out.printf("%s value 1 %s value 2 \n", row.get(0), row.get(1));
-            }
-        }
     }
 
 }
